bab74c8107049a7fdb2e3d723dad76b31479f7f8
Vijay-CIS <vijay.annamalai001@gmail.com>
2023-06-23 11:23:07 +0530

    first commit

---
+++ springBoot1/src/main/java/com/pack/springBoot1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.pack.springBoot1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
